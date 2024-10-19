24207b7ea31a28e35cc26bae1ec9375fd630cac7
Peter <maypeter0090@gmail.com>
2024-10-18 21:45:23 -0600

    implemented main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,79 @@
+import javax.lang.model.type.NullType;
+import java.util.Scanner;
+
 public class Main {
+    static void usage() {
+        System.out.println("Usage: <+, -, *, /> <number 1> <number 2>");
+        System.out.println("or, Usage: <fibonacci, binary> <number 1>");
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Testing Dockerfile");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            System.out.println("Input: ");
+            System.out.flush();
+            String input = scanner.nextLine();
+            String[] arguments = input.split(" ");
+            
+            Calculator calculator = new Calculator();
+            
+            if (arguments.length != 3 && arguments.length != 2) {
+                System.out.println("Invalid input");
+                usage();
+            }
+            
+            String operator = arguments[0];
+            int number1 = Integer.parseInt(arguments[1]);
+            int number2;
+            Integer outputInt = null;
+            String outputString = null;
+            
+            if (arguments.length == 3) {
+                number2 = Integer.parseInt(arguments[2]);
+                
+                switch (operator) {
+                    case "add":
+                    case "+": {
+                        outputInt = calculator.add(number1, number2);
+                        break;
+                    }
+                    case "subtract":
+                    case "-": {
+                        outputInt = calculator.subtract(number1, number2);
+                        break;
+                    }
+                    case "multiply":
+                    case "*": {
+                        outputInt = calculator.multiply(number1, number2);
+                        break;
+                    }
+                    case "divide":
+                    case "/": {
+                        outputInt = calculator.divide(number1, number2);
+                        break;
+                    }
+                    default:
+                        System.out.println("Invalid operator");
+                        break;
+                }
+            } else if (arguments.length == 2) {
+                switch (operator) {
+                    case "fibonacci": {
+                        outputInt = calculator.fibonacciNumberFinder(number1);
+                        break;
+                    }
+                    case "binary": {
+                        outputString = calculator.intToBinaryNumber(number1);
+                        break;
+                    }
+                }
+            }
+            
+            if (outputInt != null) {
+                System.out.println(outputInt);
+            } else {
+                System.out.println(outputString);
+            }
+        }
     }
 }
